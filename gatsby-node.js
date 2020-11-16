aaf47018c949790760b13d319d7e0b06936b133b
Kevin Jeffers <jevinkeffers@gmail.com>
2020-11-16 05:27:50 -0500

    Took out Contentful data call

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,22 +1,22 @@
-const path = require("path")
+// const path = require("path")
 
-exports.createPages = async ({ graphql, actions }) => {
-    const { createPage } = actions
-    const response = await graphql(`
-        allContentfulBlog {
-            nodes {
-                slug
-                }
-            }
-        }
-    `)
-    response.data.allContentfulBlog.edges.forEach(edge => {
-        createPage({
-            path: `/blog/${edge.node.slug}`,
-            component: path.resolve("./src/templates/blog-post.js"),
-            context: {
-                slug: edge.node.slug,
-            },
-        })
-    })
-}
+// exports.createPages = async ({ graphql, actions }) => {
+//     const { createPage } = actions
+//     const response = await graphql(`
+//         allContentfulBlog {
+//             nodes {
+//                 slug
+//                 }
+//             }
+//         }
+//     `)
+//     response.data.allContentfulBlog.edges.forEach(edge => {
+//         createPage({
+//             path: `/blog/${edge.node.slug}`,
+//             component: path.resolve("./src/templates/blog-post.js"),
+//             context: {
+//                 slug: edge.node.slug,
+//             },
+//         })
+//     })
+// }
